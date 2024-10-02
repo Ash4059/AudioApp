716c272a9169eb3aeb7f9d0b46f70de39b10b277
Ash4059 <jaiswalashish405@gmail.com>
2024-10-02 22:03:51 +0530

    Spring security configuartion in progress

--- src/main/java/com/TuneWave/AudioApp/Config/SecurityConfig.java
+++ src/main/java/com/TuneWave/AudioApp/Config/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.TuneWave.AudioApp.Config;public class SecurityConfig {
+package com.TuneWave.AudioApp.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // Disable spring security default thing
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        // Authenticate all the request
+        .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        // way to enable HTTP Basic Authentication using all the default settings provided by Spring Security
+        .httpBasic(Customizer.withDefaults())
+        // Making http request stateless. So new session will be created for each request.
+        .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return  httpSecurity.build();
+    }
+
 }
